4ec33b37f1b9aa3efd1143b34406184b78e9eff2
EmiliSena <emilisena@outlook.com>
2022-02-21 13:17:21 -0300

    Atividade 1 kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main () {
+
+                                                        ")
+
+    //Exemplo criativo de conversa
+
+    println("Henrique- Eae,blz")
+    println("Rogeerio- Blz, sim e você ")
+            println("Tudo bem, graças a Deus")
+
+
+
+
+}
